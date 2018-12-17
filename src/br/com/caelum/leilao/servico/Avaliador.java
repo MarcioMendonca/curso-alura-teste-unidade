b51d4f5ee7ad6f6103a005bcefbeee9cbcfd2c98
gx2.marcio <marcio.mendonca@agibank.com.br>
2018-12-17 12:44:51 -0200

    Primeiro teste de unidade.

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -7,18 +7,26 @@ public class Avaliador {
 
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private double lanceLimite = 10000;
 
 	public void avalia(Leilao leilao) {
 
 		for (Lance lance : leilao.getLances()) {
-			if (lance.getValor() > maiorDeTodos)
-				maiorDeTodos = lance.getValor();
-			else if (lance.getValor() < menorDeTodos)
-				menorDeTodos = lance.getValor();
-//			System.out.println(lance.getUsuario());
+			if (lance.getValor() == lanceLimite) {
+				this.lanceLimite = lance.getValor();				
+			} else {
+				if (lance.getValor() > maiorDeTodos)
+					maiorDeTodos = lance.getValor();
+				if (lance.getValor() < menorDeTodos)
+					menorDeTodos = lance.getValor();
+			}
 		}
 	}
 
+	public double getLanceLimite() {
+		return lanceLimite;
+	}
+	
 	public double getMaiorLance() {
 		return maiorDeTodos;
 	}
--- test/br/com/caelum/leilao/servico/AvaliadorTest.java
+++ test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -1,38 +1,45 @@
-package br.com.caelum.leilao.teste;
+package br.com.caelum.leilao.servico;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
 
-public class TesteAvaliador {
-	
-	public static void main(String[] args) {
-		
+public class AvaliadorTest {
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+		// parte 1: cenario
 		Usuario joao = new Usuario("Joao");
 		Usuario jose = new Usuario("Jose");
 		Usuario maria = new Usuario("Maria");
-		
+		Usuario rico = new Usuario("Rico");
+
 		Leilao leilao = new Leilao("Playstation 3 novo.");
-		
+
 		leilao.propoe(new Lance(joao, 300.0));
 		leilao.propoe(new Lance(jose, 400.0));
 		leilao.propoe(new Lance(maria, 250.0));
-		
+		leilao.propoe(new Lance(rico, 10000.0));
+
+		// parte 2: acao
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avalia(leilao);
-		
+
+		// parte 3: validacao
 		double maiorEsperado = 400;
 		double menorEsperado = 250;
+		double lanceLimite = 10000;
+		
 
-		
-		
-		System.out.println(leiloeiro.getMaiorLance());
-		System.out.println(leiloeiro.getMenorDeTodos());
-		
-		
-		System.out.println(maiorEsperado == leiloeiro.getMaiorLance());
-		System.out.println(menorEsperado == leiloeiro.getMenorDeTodos());
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorDeTodos(), 0.00001);
+		assertEquals(lanceLimite, leiloeiro.getLanceLimite(), 0.00001);
+
 	}
 
 }
